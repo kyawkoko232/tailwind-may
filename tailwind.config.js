9d123b64b65f47c8d7dae3cf16373c05a0727d83
kyawkoko232 <kyawkoko9m@gmail.com>
2024-07-04 20:11:53 +0630

    img

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,14 @@ module.exports = {
         "primary" : "#B1AFFF",
         "base" : "#F19ED2",
       },
+      fontFamily : {
+        "poppins" : "Poppins",
+        "padauk" : "Padauk",
+        "noto-mm" : "Noto Sans Myanmar",
+      },
     },
   },
-  
+
   plugins: [],
 }
 
